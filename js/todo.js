2937afb18280b5da05e984fef8af8f38cc2e3910
jangtang2 <atoa04@naver.com>
2023-04-21 15:51:34 +0900

    keep

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,8 @@ const todo_form = document.getElementById("todo-form");
 const todo_input = todo_form.querySelector("input");
 const todo_list = document.getElementById("todo-list");
 
+const todomem=[];
+
 function painttodo(newtodo){
     const li = document.createElement("li");
     const span = document.createElement("span");
@@ -18,11 +20,20 @@ function todosubmit(event){
     event.preventDefault();
     const newtodo = todo_input.value;
     todo_input.value = "";
+    todomem.push(newtodo);
     painttodo(newtodo);
+    savetodo(newtodo);
 }
 function deletetodo(event){
     const parent =event.target.parentElement;
     parent.remove();
 }
+function savetodo(){
+    localStorage.setItem("todos", JSON.stringify(todomem));
+}
 
 todo_form.addEventListener("submit", todosubmit);
+const saved_todos = localStorage.getItem("todos");
+if(saved_todos){
+    const parsedtodo = JSON.parse(saved_todos);
+}
