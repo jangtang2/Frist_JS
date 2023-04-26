a772560af13f026fc159d5b4f76c1fefd0486849
jangtang2 <atoa04@naver.com>
2023-04-26 16:59:57 +0900

    [js] - Now we can see our location and weather.

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,23 @@
+const apikey="52f4316c47965df58a8fd86aa31fb8cf";
+
 function geo_ok(position){
-    console.log(position);
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    
+    const w_url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apikey}`;
+
+    fetch(w_url)
+    .then((response) => response.json())
+    .then((data) => {
+        const weather_info = document.querySelector("#weather span:first-child");
+        const city_info = document.querySelector("#weather span:last-child");
+        city_info.innerText = data.name; 
+        weather_info.innerText = data.weather[0].main;
+    });
 }
 function geo_error(){
     alert("Please allow to check your location!");
 }
 navigator.geolocation.getCurrentPosition(geo_ok, geo_error);
+
+
